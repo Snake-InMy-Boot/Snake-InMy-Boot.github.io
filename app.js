64325d052cd433cf386da2e940b6a7e118a9db71
Snake-InMy-Boot <75350176+Snake-InMy-Boot@users.noreply.github.com>
2021-01-31 20:27:49 -0600

    UI tweaks
    made the theme selector responsive

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// Toggle Dark theme or light theme and remember the preference
+const btn = document.getElementById("theme-toggle");
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme"),
+  btn.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme"),
+  btn.classList.toggle("dark-theme");
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+  });
+
 //responsive logo
 let resize = document.querySelector(".logo");
 
 window.addEventListener("resize", function() {
-  if (window.innerWidth < 450) {
+  if (window.innerWidth < 500) {
     resize.classList.add("resize");
   } else {
     resize.classList.remove("resize");
@@ -10,11 +28,30 @@ window.addEventListener("resize", function() {
 });
 
 function campaignSelector() {
-    let nav = document.getElementById("myTopnav");
-    if (nav.className === "topnav") {
-      nav.className += " toggle";
-    } else {
-      nav.className = "topnav";
+  let nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " toggle";
+  } else {
+    nav.className = "topnav";
+  }
+}
+
+// Advanced options switch that will show hidden form inputs when toggled
+const checkbox = document.getElementById("advancedOptions");
+const hide = document.querySelectorAll(".hide");
+
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    for (var i = 0; i < hide.length; i++) {
+      hide[i].classList.remove("display-none");
+    }
+  } else {
+    for (var i = 0; i < hide.length; i++) {
+      hide[i].classList.add("display-none");
     }
   }
+});
 
+
+
+ 
--- js/select.js
+++ js/select.js
@@ -1,40 +1,3 @@
-//responsive logo
-let resize = document.querySelector(".logo");
-
-window.addEventListener("resize", function() {
-  if (window.innerWidth < 450) {
-    resize.classList.add("resize");
-  } else {
-    resize.classList.remove("resize");
-  }
-});
-
-// Responsive nav bar menu
-function campaignSelector() {
-  let nav = document.getElementById("myTopnav");
-  if (nav.className === "topnav") {
-    nav.className += " toggle";
-  } else {
-    nav.className = "topnav";
-  }
-}
-
-// Advanced options switch that will show hidden form inputs when toggled
-const checkbox = document.getElementById("advancedOptions");
-const hide = document.querySelectorAll(".hide");
-
-checkbox.addEventListener("change", function() {
-  if (this.checked) {
-    for (var i = 0; i < hide.length; i++) {
-      hide[i].classList.remove("display-none");
-    }
-  } else {
-    for (var i = 0; i < hide.length; i++) {
-      hide[i].classList.add("display-none");
-    }
-  }
-});
-
 //  formatter for numbers that should be displayed as currency
 let formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -43,203 +6,201 @@ let formatter = new Intl.NumberFormat('en-US', {
 });
 
 function calculate() {
-  week1();
-  week2();
-  total();
-}
-
-function week1() {
-const scheduleSelector = document.getElementById("scheduleSelector");
-const shiftSelector = document.getElementById("shiftSelector");
-const week = document.getElementById("week1");
-const input = week.querySelectorAll("input");
-let units = input[0].value;
-let select = input[1].value;
-let apple = input[2].value;
-let hours = input[3].value;
-let uph = input[4];
-let hourlyRate = input[5];
-let dialPay = input[6];
-let attendance = input[7];
-let commission = input[8];
-let total = input[9];
-let scheduled = null;
-const over200 =  units - Math.ceil(hours * 0.50)
-
-if (units !== undefined && hours !== undefined) {
-  uph.value = (units / hours).toFixed(2);
-  dialPay.value = hours * 10;
-
-  // Calculates agent's attendance bonus based on FT/PT and Day/Night and at least 97.5%
-  if (scheduleSelector.checked == true) {
-    scheduled = 20;
-    if (hours / scheduled >= 0.975) {
-      attendance.value = 1;    
-    } else {
-      attendance.value = 0;
-    }
-  } else {
-    scheduled = 40;
-    if (hours / scheduled >= 0.975) {
-      if (shiftSelector.checked == true) {
-        attendance.value = 2;
+  function week1() {
+    const scheduleSelector = document.getElementById("scheduleSelector");
+    const shiftSelector = document.getElementById("shiftSelector");
+    const week = document.getElementById("week1");
+    const input = week.querySelectorAll("input");
+    let units = input[0].value;
+    let select = input[1].value;
+    let apple = input[2].value;
+    let hours = input[3].value;
+    let uph = input[4];
+    let hourlyRate = input[5];
+    let dialPay = input[6];
+    let attendance = input[7];
+    let commission = input[8];
+    let total = input[9];
+    let scheduled = null;
+    const over200 =  units - Math.ceil(hours * 0.50)
+    
+    if (units !== undefined && hours !== undefined) {
+      uph.value = (units / hours).toFixed(2);
+      dialPay.value = hours * 10;
+    
+      // Calculates agent's attendance bonus based on FT/PT and Day/Night and at least 97.5%
+      if (scheduleSelector.checked == true) {
+        scheduled = 20;
+        if (hours / scheduled >= 0.975) {
+          attendance.value = 1;    
+        } else {
+          attendance.value = 0;
+        }
       } else {
-        attendance.value = 1;
+        scheduled = 40;
+        if (hours / scheduled >= 0.975) {
+          if (shiftSelector.checked == true) {
+            attendance.value = 2;
+          } else {
+            attendance.value = 1;
+          }
+        } else {
+          attendance.value = 0;
+        }
+      }
+    }
+    
+    if (attendance.value != 0) {
+      if (uph.value >= 0.50) {
+        commission.value = 8.95 + over200 * 5 / hours;
+      } else if (uph.value >= 0.48) {
+        commission.value = 7.70; 
+      } else if (uph.value >= 0.45) {
+        commission.value = 6.45; 
+      } else if (uph.value >= 0.43) {
+        commission.value = 5.05; 
+      } else if (uph.value >= 0.40) {
+        commission.value = 4.50; 
+      } else if (uph.value >= 0.38) {
+        commission.value = 4.05;
+      } else if (uph.value >= 0.35) {
+        commission.value = 3.15;
+      } else if (uph.value >= 0.33) {
+        commission.value = 3.00; 
+      } else if (uph.value >= 0.30) {
+        commission.value = 2.90;
+      } else if (uph.value >= 0.28) {
+        commission.value = 2.75;
+      } else if (uph.value >= 0.25) {
+        commission.value = 2.65;
+      } else {
+        commission.value = 0;  
       }
     } else {
-      attendance.value = 0;
+        commission.value = 0,
+        hourlyRate.value = 10;
     }
-  }
-}
-
-if (attendance.value != 0) {
-  if (uph.value >= 0.50) {
-    commission.value = 8.95 + over200 * 5 / hours;
-  } else if (uph.value >= 0.48) {
-    commission.value = 7.70; 
-  } else if (uph.value >= 0.45) {
-    commission.value = 6.45; 
-  } else if (uph.value >= 0.43) {
-    commission.value = 5.05; 
-  } else if (uph.value >= 0.40) {
-    commission.value = 4.50; 
-  } else if (uph.value >= 0.38) {
-    commission.value = 4.05;
-  } else if (uph.value >= 0.35) {
-    commission.value = 3.15;
-  } else if (uph.value >= 0.33) {
-    commission.value = 3.00; 
-  } else if (uph.value >= 0.30) {
-    commission.value = 2.90;
-  } else if (uph.value >= 0.28) {
-    commission.value = 2.75;
-  } else if (uph.value >= 0.25) {
-    commission.value = 2.65;
-  } else {
-    commission.value = 0;  
-  }
-} else {
-    commission.value = 0,
-    hourlyRate.value = 10;
-}
-
-if (attendance.value >= 1) {
-commission.value = commission.value * hours + apple * 5 + select * 7; 
-}
-
-attendance.value = attendance.value * hours;
-total.value = parseFloat(dialPay.value) + parseFloat(attendance.value) + parseFloat(commission.value);
-hourlyRate.value = total.value / hours;
-
-
-// Formatting values to currency
-hourlyRate.value = formatter.format(hourlyRate.value);
-dialPay.value = formatter.format(dialPay.value);
-attendance.value = formatter.format(attendance.value);
-commission.value = formatter.format(commission.value);
-
-}
-
-function week2() {
-const scheduleSelector = document.getElementById("scheduleSelector");
-const shiftSelector = document.getElementById("shiftSelector");
-const week = document.getElementById("week2");
-const input = week.querySelectorAll("input");
-let units = input[0].value;
-let select = input[1].value;
-let apple = input[2].value;
-let hours = input[3].value;
-let scheduled = null;
-let uph = input[4];
-let hourlyRate = input[5];
-let dialPay = input[6];
-let attendance = input[7];
-let commission = input[8];
-let total = input[9];
-const over200 =  units - Math.ceil(hours * 0.50)
-
-if (units !== undefined && hours !== undefined) {
-  uph.value = (units / hours).toFixed(2);
-  dialPay.value = hours * 10;
-
-  // Calculates agent's attendance bonus based on FT/PT and Day/Night and at least 97.5%
-  if (scheduleSelector.checked == true) {
-    scheduled = 20;
-    if (hours / scheduled >= 0.975) {
-      attendance.value = 1;    
-    } else {
-      attendance.value = 0;
+    
+    if (attendance.value >= 1) {
+    commission.value = commission.value * hours + apple * 5 + select * 7; 
     }
-  } else {
-    scheduled = 40;
-    if (hours / scheduled >= 0.975) {
-      if (shiftSelector.checked == true) {
-        attendance.value = 2;
+    
+    attendance.value = attendance.value * hours;
+    total.value = parseFloat(dialPay.value) + parseFloat(attendance.value) + parseFloat(commission.value);
+    hourlyRate.value = total.value / hours;
+    
+    
+    // Formatting values to currency
+    hourlyRate.value = formatter.format(hourlyRate.value);
+    dialPay.value = formatter.format(dialPay.value);
+    attendance.value = formatter.format(attendance.value);
+    commission.value = formatter.format(commission.value);
+    
+    }
+    
+    function week2() {
+    const scheduleSelector = document.getElementById("scheduleSelector");
+    const shiftSelector = document.getElementById("shiftSelector");
+    const week = document.getElementById("week2");
+    const input = week.querySelectorAll("input");
+    let units = input[0].value;
+    let select = input[1].value;
+    let apple = input[2].value;
+    let hours = input[3].value;
+    let scheduled = null;
+    let uph = input[4];
+    let hourlyRate = input[5];
+    let dialPay = input[6];
+    let attendance = input[7];
+    let commission = input[8];
+    let total = input[9];
+    const over200 =  units - Math.ceil(hours * 0.50)
+    
+    if (units !== undefined && hours !== undefined) {
+      uph.value = (units / hours).toFixed(2);
+      dialPay.value = hours * 10;
+    
+      // Calculates agent's attendance bonus based on FT/PT and Day/Night and at least 97.5%
+      if (scheduleSelector.checked == true) {
+        scheduled = 20;
+        if (hours / scheduled >= 0.975) {
+          attendance.value = 1;    
+        } else {
+          attendance.value = 0;
+        }
       } else {
-        attendance.value = 1;
+        scheduled = 40;
+        if (hours / scheduled >= 0.975) {
+          if (shiftSelector.checked == true) {
+            attendance.value = 2;
+          } else {
+            attendance.value = 1;
+          }
+        } else {
+          attendance.value = 0;
+        }
+      }
+    }
+    
+    if (attendance.value != 0) {
+      if (uph.value >= 0.50) {
+        commission.value = 8.95 + over200 * 5 / hours;
+      } else if (uph.value >= 0.48) {
+        commission.value = 7.70; 
+      } else if (uph.value >= 0.45) {
+        commission.value = 6.45; 
+      } else if (uph.value >= 0.43) {
+        commission.value = 5.05; 
+      } else if (uph.value >= 0.40) {
+        commission.value = 4.50; 
+      } else if (uph.value >= 0.38) {
+        commission.value = 4.05;
+      } else if (uph.value >= 0.35) {
+        commission.value = 3.15;
+      } else if (uph.value >= 0.33) {
+        commission.value = 3.00; 
+      } else if (uph.value >= 0.30) {
+        commission.value = 2.90;
+      } else if (uph.value >= 0.28) {
+        commission.value = 2.75;
+      } else if (uph.value >= 0.25) {
+        commission.value = 2.65;
+      } else {
+        commission.value = 0;  
       }
     } else {
-      attendance.value = 0;
+      commission.value = 0,
+      hourlyRate.value = 10;
     }
-  }
+    
+    if (attendance.value >= 1) {
+      commission.value = commission.value * hours + apple * 5 + select * 7; 
+      }
+      
+    attendance.value = attendance.value * hours;
+    total.value = parseFloat(dialPay.value) + parseFloat(attendance.value) + parseFloat(commission.value);
+    hourlyRate.value = total.value / hours;
+    
+    // Formatting values to currency
+    hourlyRate.value = formatter.format(hourlyRate.value);
+    dialPay.value = formatter.format(dialPay.value);
+    attendance.value = formatter.format(attendance.value);
+    commission.value = formatter.format(commission.value);
+    
+    }
+    
+    function total() {
+      const input = document.querySelectorAll("input");
+      let total1 = input[12];
+      let total2 = input[22];
+      let payPeriod = input[23];
+     
+      payPeriod.value = parseFloat(total1.value) + parseFloat(total2.value);
+    
+      total1.value = formatter.format(total1.value);
+      total2.value = formatter.format(total2.value);
+      payPeriod.value = formatter.format(payPeriod.value);
+    }
 }
 
-if (attendance.value != 0) {
-  if (uph.value >= 0.50) {
-    commission.value = 8.95 + over200 * 5 / hours;
-  } else if (uph.value >= 0.48) {
-    commission.value = 7.70; 
-  } else if (uph.value >= 0.45) {
-    commission.value = 6.45; 
-  } else if (uph.value >= 0.43) {
-    commission.value = 5.05; 
-  } else if (uph.value >= 0.40) {
-    commission.value = 4.50; 
-  } else if (uph.value >= 0.38) {
-    commission.value = 4.05;
-  } else if (uph.value >= 0.35) {
-    commission.value = 3.15;
-  } else if (uph.value >= 0.33) {
-    commission.value = 3.00; 
-  } else if (uph.value >= 0.30) {
-    commission.value = 2.90;
-  } else if (uph.value >= 0.28) {
-    commission.value = 2.75;
-  } else if (uph.value >= 0.25) {
-    commission.value = 2.65;
-  } else {
-    commission.value = 0;  
-  }
-} else {
-  commission.value = 0,
-  hourlyRate.value = 10;
-}
 
-if (attendance.value >= 1) {
-  commission.value = commission.value * hours + apple * 5 + select * 7; 
-  }
-  
-attendance.value = attendance.value * hours;
-total.value = parseFloat(dialPay.value) + parseFloat(attendance.value) + parseFloat(commission.value);
-hourlyRate.value = total.value / hours;
-
-// Formatting values to currency
-hourlyRate.value = formatter.format(hourlyRate.value);
-dialPay.value = formatter.format(dialPay.value);
-attendance.value = formatter.format(attendance.value);
-commission.value = formatter.format(commission.value);
-
-}
-
-function total() {
-  const input = document.querySelectorAll("input");
-  let total1 = input[12];
-  let total2 = input[22];
-  let payPeriod = input[23];
- 
-  payPeriod.value = parseFloat(total1.value) + parseFloat(total2.value);
-
-  total1.value = formatter.format(total1.value);
-  total2.value = formatter.format(total2.value);
-  payPeriod.value = formatter.format(payPeriod.value);
-}
